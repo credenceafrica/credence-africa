6633c4069e19b8b8b6f19ddce4f78a9cf5232267
CREDENCE AFRICA <credence.africa@gmail.com>
2026-02-24 06:26:50 +0000

    Add separate firebase configs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,3 +6,30 @@ export const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+export const perspectiveFirebaseConfig = {
+  apiKey: "AIzaSyBJyoJA15KlZT2N1XQ6cGmY7KduKDu1WgQ",
+  authDomain: "studio-4503671089-d9436.firebaseapp.com",
+  projectId: "studio-4503671089-d9436",
+  storageBucket: "studio-4503671089-d9436.firebasestorage.app",
+  messagingSenderId: "1061210512147",
+  appId: "1:1061210512147:web:21cbad04e05c8841eb58e6"
+};
+
+export const engageFirebaseConfig = {
+  apiKey: "AIzaSyBJyoJA15KlZT2N1XQ6cGmY7KduKDu1WgQ",
+  authDomain: "studio-4503671089-d9436.firebaseapp.com",
+  projectId: "studio-4503671089-d9436",
+  storageBucket: "studio-4503671089-d9436.firebasestorage.app",
+  messagingSenderId: "1061210512147",
+  appId: "1:1061210512147:web:21cbad04e05c8841eb58e6"
+};
+
+export const instituteFirebaseConfig = {
+  apiKey: "AIzaSyAWLVo0et7q4KZwM-t1V3mDEmQvjoV2dJg",
+  authDomain: "studio-3902174597-e6704.firebaseapp.com",
+  projectId: "studio-3902174597-e6704",
+  storageBucket: "studio-3902174597-e6704.firebasestorage.app",
+  messagingSenderId: "1030639319823",
+  appId: "1:1030639319823:web:886ecd9772401f77599c7a"
+};
