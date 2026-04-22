955c7c20cb2f27d7cf3f8e78922ebec639c77149
CREDENCE AFRICA <credence.africa@gmail.com>
2026-04-22 10:32:12 +0000

    Change logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export function Logo({ className }: { className?: string }) {
   return (
     <img
-      src="/logo.png"
+      src="/logo-f.jpg"
       alt="Credence Africa logo"
       className={cn("h-10 w-10", className)}
     />
