d19336d588eee8bcd7a2d33e927a94655c7cb89a
piusgori <piusgori@gmail.com>
2026-09-14 08:58:10 +0300

    Update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -41,9 +41,9 @@ export function Footer() {
           <div>
             <h3 className="font-semibold uppercase text-foreground/80">Contact</h3>
             <ul className="mt-4 space-y-2 text-sm">
-              <li><a href="mailto:connect@credence.africa" className="text-muted-foreground hover:text-primary hover:underline">connect@credence.africa</a></li>
-              <li><a href="tel:+254719468240" className="text-muted-foreground hover:text-primary hover:underline">+254 719 468 240</a></li>
-              <li><a href="https://maps.app.goo.gl/EeS5D837UZnSDsEZ7" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary hover:underline">Applewood Adams, Ngong Road, Nairobi</a></li>
+              <li><a href="mailto:connect@credence.africa" className="text-muted-foreground hover:text-primary hover:underline">Email: connect@credence.africa</a></li>
+              <li><a href="tel:+254719468240" className="text-muted-foreground hover:text-primary hover:underline">Office Number: +254 719 468 240</a></li>
+              <li><a href="https://maps.app.goo.gl/EeS5D837UZnSDsEZ7" target="_blank" rel="noopener noreferrer" className="text-muted-foreground hover:text-primary hover:underline">Location: 11th Suite 1102, Applewood Adams, Ngong Road, Nairobi, Kenya</a></li>
             </ul>
           </div>
 
